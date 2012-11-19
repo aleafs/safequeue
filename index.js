7dc19a9f9d1702be6cd53cabd93b6f92b4e39540
aleafs <zhangxc83@gmail.com>
2012-11-20 07:53:51 +0800

    First init

---
+++ index.js
@@ -0,0 +1,6 @@
+/* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
+
+"use strict";
+
+module.exports = require(__dirname + '/lib/queue.js');
+
---
+++ test/queue.test.js
@@ -0,0 +1,5 @@
+/* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
+
+"use strict";
+
+var Queue = require(__dirname + '/../');
